040e0814c11a4e3b702adf3eda6076d3ae42aca4
Gofile <yantonuri917@gmail.com>
2026-05-29 13:40:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
-window.API_SETTINGS = {
+window.API_CONFIG = {
     URL: "https://xgmsumukxuwjpztbeuwl.supabase.co/rest/v1",
-    TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnbXN1bXVreHV3anB6dGJldXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NTE1MzYsImV4cCI6MjA5NTQyNzUzNn0.25t9t71O0O2D11l7GRXtpvOgC2sllXE3xB4nsRKK4oE" 
+    KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnbXN1bXVreHV3anB6dGJldXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NTE1MzYsImV4cCI6MjA5NTQyNzUzNn0.25t9t71O0O2D11l7GRXtpvOgC2sllXE3xB4nsRKK4oE"
 };
