62217872cdb497562de938fe29e65318ae65f28d
Gofile <yantonuri917@gmail.com>
2026-05-29 13:04:38 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.API_SETTINGS = {
+    URL: "https://xgmsumukxuwjpztbeuwl.supabase.co",
+    TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnbXN1bXVreHV3anB6dGJldXdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NTE1MzYsImV4cCI6MjA5NTQyNzUzNn0.25t9t71O0O2D11l7GRXtpvOgC2sllXE3xB4nsRKK4oE" 
+};
