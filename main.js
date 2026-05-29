ffdd798079ee763e87ad71e630e6086330607627
Gofile <yantonuri917@gmail.com>
2026-05-29 13:06:20 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = window.APP_CONFIG.SUPABASE_URL;
+const SUPABASE_KEY = window.APP_CONFIG.SUPABASE_KEY;
+
+// Kode Anda selanjutnya tetap berjalan normal
+async function fetchVideosFromDatabase() {
+    // ... fungsi Anda tetap sama
+}
